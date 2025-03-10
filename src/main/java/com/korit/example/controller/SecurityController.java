9b8e82e9533b2db995cfa4bcaa55583131b4432f
Artair-kr <hjs6855@naver.com>
2025-03-10 17:15:40 +0900

    비밀번호 암호화 25-03-10 17:30

--- src/main/java/com/korit/example/controller/SecurityController.java
+++ src/main/java/com/korit/example/controller/SecurityController.java
@@ -1,12 +1,16 @@
 package com.korit.example.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.korit.example.dto.SignUpRequestDto;
 import com.korit.example.provider.JwtProvider;
+import com.korit.example.service.SecurityService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,23 +20,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/security")
 @RequiredArgsConstructor
 public class SecurityController {
-  
+
+  private final SecurityService securityService;
   private final JwtProvider jwtProvider;
-
-  @GetMapping("/jwt")
-  public String getJwt(
-    @RequestParam("name") String name
-  ) {
-    String jwt = jwtProvider.create(name);
-    return jwt;
+    private SignUpRequestDto requestBody;
+  
+    @GetMapping("/jwt")
+    public String getJwt(
+      @RequestParam("name") String name
+    ) {
+      String jwt = jwtProvider.create(name);
+      return jwt;
+    }
+  
+    @PostMapping("/jwt")
+    public String validateJwt(
+      @RequestBody String jwt
+    ) {
+      String subject = jwtProvider.validate(jwt);
+      return subject;
+    }
+  
+    @PostMapping("/sign-up")
+    public ResponseEntity<String> signUp(
+      @RequestBody @Valid SignUpRequestDto requestBody
+    ){
+      ResponseEntity<String> response = securityService.signUp(requestBody);
+    return response;
   }
-
-  @PostMapping("/jwt")
-  public String validateJwt(
-    @RequestBody String jwt
-  ) {
-    String subject = jwtProvider.validate(jwt);
-    return subject;
-  }
-
+  
 }
